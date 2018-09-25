3808c3bfcb709bc08b41d44194066042610c1e06
Tamer <tamer.abdulghani@gmail.com>
2018-09-25 23:57:53 +0200

    Initializing spring boot maven project

---
+++ software-quality-platform/src/main/java/com/miage/sqp/IndexController.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.miage.sqp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ * @author Tamer
+ */
+@Controller
+public class IndexController {
+
+    @GetMapping("/home")
+    public String index(Model model) {
+        return "index";
+    }
+}
