0dc47c816477fe6fcde5885135d4dd387a14a151
Tamer <tamer.abdulghani@gmail.com>
2018-10-06 16:37:36 +0200

    Configure mysql auto creation script

--- software-quality-platform/src/main/java/com/miage/sqp/Application.java
+++ software-quality-platform/src/main/java/com/miage/sqp/Application.java
@@ -13,11 +13,19 @@ import java.util.Arrays;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableAutoConfiguration
 @SpringBootApplication
+@ComponentScan({"com.miage.*"})
+@EntityScan("com.miage.*")
+@EnableJpaRepositories("com.miage.*")
 public class Application {
 
     public static void main(String[] args) {
